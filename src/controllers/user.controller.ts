757abdaa4f499dda3aac5e37fd11bd16e670e739
Tâm Nguyễn <horashidev@gmail.com>
2025-05-29 08:23:31 +0700

    create create-user form

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,4 +4,8 @@ const getHomePage = (req:Request, res:Response) => {
     res.render('home', {name: 'Tam', age: 21})
 }
 
-export {getHomePage}
+const getCreateUserPage = (req:Request, res:Response) => {
+    res.render('create-user')
+}
+
+export {getHomePage, getCreateUserPage}
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response, Express} from 'express'
-import { getHomePage } from '../controllers/user.controller'
+import { getCreateUserPage, getHomePage } from '../controllers/user.controller'
 const router = express.Router()
 
 const webRoute = (app: Express) => {
 
     router.get('/', getHomePage)
+
+    router.get('/create-user', getCreateUserPage)
 
     app.use('/', router)
 }
